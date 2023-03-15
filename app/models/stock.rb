541b0408b1c3b4c6b800a1240bf87ed0389c1417
Binh Do <binh@worldfront.co>
2023-03-16 09:23:48 +1300

    Setup User_stocks resource

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     Alphavantage.configure do |config|
       config.api_key = Rails.application.credentials.alphavantage[:api_key]
