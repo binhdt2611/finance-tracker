b61eda2e5a7721b23d8249660cfbac93df96fac8
Binh Do <binh@worldfront.co>
2023-03-25 14:52:00 +1300

    Add friend list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+  
+  def friends
+    @tracked_friendships = current_user.friends
+  end
 end
